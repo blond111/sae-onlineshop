9ba3c2da9c957ab3549775110f4fb34f20413d67
Babsi Kritsch <babsi@kritsch.at>
2018-01-16 00:04:47 +0100

    Abgegeben

--- includes/dbconnect.php
+++ includes/dbconnect.php
@@ -1,6 +1,6 @@
 <?php
 
-$dblink = mysqli_connect("localhost", "root", "ZwRTF4547Y%*gZgauVD!3Go0H3&g6FFGEDewZNxHbH%byIai", "onlineshop");
+$dblink = mysqli_connect("localhost", "root", "", "onlineshop");
 mysqli_query($dblink, "SET names UTF8");
 
 if (!$dblink) {
